369973037865a2c1275fb0300171c1eb00680794
OdysseasPatilis <odysseasptls@gmail.com>
2020-05-25 20:02:00 +0300

    initial commit

---
+++ src/test/java/patis/odysspring/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package patis.odysspring.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
