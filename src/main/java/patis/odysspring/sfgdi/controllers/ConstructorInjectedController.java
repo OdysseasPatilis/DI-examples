4ec0cfb604144aabc99f33952f01edb4cf5a6db6
OdysseasPatilis <odysseasptls@gmail.com>
2020-05-27 12:27:00 +0300

    removed the Autowire annotation in the constructor injected controller because it is unnecessary

--- src/main/java/patis/odysspring/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/patis/odysspring/sfgdi/controllers/ConstructorInjectedController.java
@@ -8,7 +8,6 @@ import patis.odysspring.sfgdi.services.GreetingService;
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
-    @Autowired
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
