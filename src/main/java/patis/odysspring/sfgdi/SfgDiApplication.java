34783fb868070483b076f5ee98ce7766bd801589
OdysseasPatilis <odysseasptls@gmail.com>
2020-05-27 12:57:58 +0300

    added the Primary annotation.If there is no Qualifier the Primary Bean is used

--- src/main/java/patis/odysspring/sfgdi/SfgDiApplication.java
+++ src/main/java/patis/odysspring/sfgdi/SfgDiApplication.java
@@ -14,8 +14,9 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx= SpringApplication.run(SfgDiApplication.class, args);
 		MyController myController=(MyController) ctx.getBean("myController");
-		String greeting =myController.dayHello();
-		System.out.println(greeting);
+
+		System.out.println("--------------Primary Bean");
+		System.out.println(myController.dayHello());
 		System.out.println("--------------Property");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
--- src/main/java/patis/odysspring/sfgdi/controllers/MyController.java
+++ src/main/java/patis/odysspring/sfgdi/controllers/MyController.java
@@ -1,12 +1,17 @@
 package patis.odysspring.sfgdi.controllers;
 
 import org.springframework.stereotype.Controller;
+import patis.odysspring.sfgdi.services.GreetingService;
 
 @Controller
 public class MyController {
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     public String dayHello(){
-        System.out.println("Hello World");
-        return "Hi folks!!!";
+        return greetingService.sayGreeting();
     }
 }
